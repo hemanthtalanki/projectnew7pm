01274dfce789e1b15741bfb35affb9f38a8ca385
hemanthtalanki <hemanthtalanki.44@gmail.com>
2021-12-07 08:31:08 +0530

    new project

---
+++ src/org/hcl/Employee.java
@@ -0,0 +1,18 @@
+package org.hcl;
+
+public class Employee {
+	
+	public void add(int a,int b) {
+		int sum =a+b;
+		System.out.println(sum);
+
+	}
+	
+	
+	public static void main(String[] args) {
+		Employee e =new Employee();
+		e.add(10, 20);
+		
+	}
+
+}
